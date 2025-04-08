07289a267760f74084eb9f254392b9e5a8ea0f28
mohamedACHOURI <mahometashoury@gmail.com>
2025-04-08 23:45:55 +0100

    préparation de la page index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<style>
+    table,th,td {
+       border: 1px solid black !important;
+    }
+</style>
+</head>
+<body>
+    <?php
+        include 'etudiant.php';
+        $et1 = new etudiant("Aymen", [11, 13, 18, 7,10,13,2,5,1]);
+        $et2= new etudiant("skander", [15, 9, 8, 16]);
+        $et=[$et1,$et2];
+        $et1->print_all($et);
+        echo "<br>";
+        
+        ?>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
